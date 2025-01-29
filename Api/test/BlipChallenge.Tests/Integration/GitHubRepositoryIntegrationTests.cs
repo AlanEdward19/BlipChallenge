1ef3413809e8e830c40df0edc8c7022a285a0614
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2025-01-29 19:35:31 -0300

    test: added invalid cases

--- Api/test/BlipChallenge.Tests/Integration/GitHubRepositoryIntegrationTests.cs
+++ Api/test/BlipChallenge.Tests/Integration/GitHubRepositoryIntegrationTests.cs
@@ -78,4 +78,109 @@ public class GitHubRepositoryIntegrationTests
 
         #endregion
     }
+    
+    [Theory]
+    [InlineData("C#", 3)]
+    [InlineData("C#", 4)]
+    [InlineData("Java", 1)]
+    [InlineData("C#", 5)]
+    [InlineData("Java", 3)]
+    [InlineData("Python", 2)]
+    [InlineData("Python", 1)]
+    public async Task
+        GetRepositoryQueryHandler_ReturnsUserRepositories_WhenQueryIsOrganizationType_WithCorrectNumberOfRepositoriesAndCorrectProgrammingLanguage(
+            string programmingLanguage, int rows)
+    {
+        #region Arrange
+
+        GetRepositoryQuery query = new GetRepositoryQuery
+        {
+            ProgrammingLanguage = programmingLanguage,
+            Rows = rows,
+            Type = ERepositoryType.Organization,
+            UserName = "organization"
+        };
+
+        #endregion
+
+        #region Act
+
+        var result = await _repositoryQueryHandler.HandleAsync(query, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+        Assert.Equal(rows, result.Count);
+        Assert.All(result, x => Assert.Equal(programmingLanguage, x.Language));
+        
+        for (int i = 0; i < result.Count - 1; i++)
+            Assert.True(result[i].CreatedAt <= result[i + 1].CreatedAt);
+
+        #endregion
+    }
+    
+    [Theory]
+    [InlineData(null, 3)]
+    [InlineData("", 3)]
+    [InlineData("C#", -1)]
+    [InlineData("C#", 0)]
+    public async Task GetRepositoryQueryHandler_ReturnsEmptyList_WhenQueryIsInvalid(string programmingLanguage, int rows)
+    {
+        #region Arrange
+
+        GetRepositoryQuery query = new GetRepositoryQuery
+        {
+            ProgrammingLanguage = programmingLanguage,
+            Rows = rows,
+            Type = ERepositoryType.User,
+            UserName = "user"
+        };
+
+        #endregion
+
+        #region Act
+
+        var result = await _repositoryQueryHandler.HandleAsync(query, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+        Assert.Empty(result);
+
+        #endregion
+    }
+
+    [Theory]
+    [InlineData(null, 3)]
+    [InlineData("", 3)]
+    [InlineData("C#", -1)]
+    [InlineData("C#", 0)]
+    public async Task GetRepositoryQueryHandler_ReturnsEmptyList_WhenQueryIsInvalid_Organization(string programmingLanguage, int rows)
+    {
+        #region Arrange
+
+        GetRepositoryQuery query = new GetRepositoryQuery
+        {
+            ProgrammingLanguage = programmingLanguage,
+            Rows = rows,
+            Type = ERepositoryType.Organization,
+            UserName = "organization"
+        };
+
+        #endregion
+
+        #region Act
+
+        var result = await _repositoryQueryHandler.HandleAsync(query, CancellationToken.None);
+
+        #endregion
+
+        #region Assert
+
+        Assert.Empty(result);
+
+        #endregion
+    }
 }
